b03d4fadbd50e4610c8145d3fa824c068becc277
Yongyang Yu <yuyongyang800@gmail.com>
2014-07-14 17:13:25 -0400

    fix a bug

--- python/test_64bit_bvg.py
+++ python/test_64bit_bvg.py
@@ -6,7 +6,7 @@ data = sys.argv[1]
 G = bvg.BVGraph(data, 0) # load with offset step = 0 (iteration)
 print '#nodes = ' + str(G.nverts) + ', #edges = ' + str(G.nedges)
 edges_and_degrees = G.edges_and_degrees()
-for (src, dst, degree) in edges_and_degress:
+for (src, dst, degree) in edges_and_degrees:
     if degree == 1 and dst != src + 1:
         print 'error: reading 64bit bvgraph'
         break
