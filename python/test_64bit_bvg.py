3baa6cb60d965d03f3fb1f1de0b516a563302523
Yongyang Yu <yuyongyang800@gmail.com>
2014-07-14 17:11:25 -0400

    add testcase for python interface

---
+++ python/test_64bit_bvg.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+import bvg
+import sys
+
+data = sys.argv[1]
+G = bvg.BVGraph(data, 0) # load with offset step = 0 (iteration)
+print '#nodes = ' + str(G.nverts) + ', #edges = ' + str(G.nedges)
+edges_and_degrees = G.edges_and_degrees()
+for (src, dst, degree) in edges_and_degress:
+    if degree == 1 and dst != src + 1:
+        print 'error: reading 64bit bvgraph'
+        break
+    elif src > 1000:
+        print 'Python: testing 64-bit bvgraph sequential read ... passed!'
+        break
