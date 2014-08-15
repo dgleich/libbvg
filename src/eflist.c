6a9b504d3a0db2212c245934d27e2f6b1a7a83da
Yongyang Yu <yuyongyang800@gmail.com>
2014-08-15 11:05:58 -0400

    tab error

--- src/eflist.c
+++ src/eflist.c
@@ -426,6 +426,7 @@ int bit_array_free(bit_array *ptr)
 
 int eflist_init(elias_fano_list *ef, uint64_t num_elements, uint64_t largest)
 {
+    *ef = {0};
     ef->size = num_elements;
     (ef->lower).curr = 0;
     (ef->upper).curr = 0;
